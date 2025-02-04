2f0671d5f9df818c328fdd3a10bddecbd2d3b210
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-04 23:15:45 +0000

    Adding loading component.

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <Loader2 className="mx-auto my-6 flex h-dvh animate-spin items-center justify-center" />
+  );
+}
