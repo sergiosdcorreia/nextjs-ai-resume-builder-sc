b93eb0a1fa8b9d36c7602c847ed1a7675dbc83f0
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-06 13:20:27 +0000

    Fetching resumes to display in the resumes page.

---
+++ app/(main)/resumes/ResumeItem.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import ResumePreview from "@/components/ResumePreview";
+import { ResumeServerData } from "@/lib/types";
+import { mapToResumeValues } from "@/lib/utils";
+import { formatDate } from "date-fns";
+import Link from "next/link";
+
+interface ResumeItemProps {
+  resume: ResumeServerData;
+}
+
+export default function ResumeItem({ resume }: ResumeItemProps) {
+  const wasUpdated = resume.updatedAt !== resume.createdAt;
+
+  return (
+    <div className="group rounded-lg border border-transparent bg-secondary p-3 transition-colors hover:border-border">
+      <Link
+        href={`/editor?resumeId=${resume.id}`}
+        className="inline-block w-full text-center"
+      >
+        <p className="line-clamp-1 font-semibold">
+          {resume.title || "No title"}
+        </p>
+        {resume.description && (
+          <p className="line-clamp-2 text-sm">{resume.description}</p>
+        )}
+        <p className="text-xs text-muted-foreground">
+          {wasUpdated ? "Updated" : "Created"} on{" "}
+          {formatDate(resume.updatedAt, "MMM d, yyyy h:mm a")}
+        </p>
+      </Link>
+      <Link
+        href={`/editor?resumeId=${resume.id}`}
+        className="relative inline-block w-full"
+      >
+        <ResumePreview
+          resumeData={mapToResumeValues(resume)}
+          className="overflow-hidden shadow-sm group-hover:shadow-lg"
+        />
+        <div className="absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-white to-transparent" />
+      </Link>
+    </div>
+  );
+}
