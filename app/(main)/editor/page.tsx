a1180f2b2e66649e88a688242a05bfab457e57a3
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-06 15:58:40 +0000

    Force dynamic editor page.

--- app/(main)/editor/page.tsx
+++ app/(main)/editor/page.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
   title: "Design your resume",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Page({ searchParams }: PageProps) {
   const { resumeId } = await searchParams;
 
