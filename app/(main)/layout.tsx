5bcdf3ab3faa4d8e90eee1ebc488ae0103468486
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-10 02:25:36 +0000

    Continuing on subscription levels, creating a subscription provider.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import Navbar from "./Navbar";
 import PremiumModal from "@/components/Premium/PremiumModal";
+import { auth } from "@clerk/nextjs/server";
+import { getUserSubscriptionLevel } from "@/lib/subscription";
+import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function layout({ children }: Props) {
+export default async function layout({ children }: Props) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
+
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      {children}
-      <PremiumModal />
-    </div>
+    <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        {children}
+        <PremiumModal />
+      </div>
+    </SubscriptionLevelProvider>
   );
 }
