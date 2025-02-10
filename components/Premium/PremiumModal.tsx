566352da49240f15d94abb5213480919829584f3
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-10 00:15:47 +0000

    Adding Stripe and checkout pages for premium subscriptions.

--- components/Premium/PremiumModal.tsx
+++ components/Premium/PremiumModal.tsx
@@ -4,6 +4,9 @@ import { Check } from "lucide-react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { Button } from "../ui/button";
 import usePremiumModal from "@/hooks/usePremiumModal";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import { createCheackoutSession } from "./actions";
 
 const premiumFeatures = ["AI tools", "Up to 3 resumes"];
 const premiumPlusFeatures = [
@@ -13,8 +16,36 @@ const premiumPlusFeatures = [
 
 export default function PremiumModal() {
   const { open, setOpen } = usePremiumModal();
+
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handlePremiumClick(priceId: string) {
+    try {
+      setLoading(true);
+      const redirectUrl = await createCheackoutSession(priceId);
+      window.location.href = redirectUrl;
+    } catch (error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!loading) {
+          setOpen(open);
+        }
+      }}
+    >
       <DialogContent className="max-w-2xl">
         <DialogHeader className="w-full bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent">
           <DialogTitle className="text-center text-2xl">
@@ -34,7 +65,17 @@ export default function PremiumModal() {
                   </li>
                 ))}
               </ul>
-              <Button variant="secondary">Get Premium</Button>
+              <Button
+                variant="secondary"
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_3_MONTHS!,
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium
+              </Button>
             </div>
             <div className="mx-6 border-l" />
             <div className="flex w-1/2 flex-col space-y-5">
@@ -49,7 +90,17 @@ export default function PremiumModal() {
                   </li>
                 ))}
               </ul>
-              <Button variant="premium">Get Premium Plus</Button>
+              <Button
+                variant="premium"
+                onClick={() =>
+                  handlePremiumClick(
+                    process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_3_MONTHS!,
+                  )
+                }
+                disabled={loading}
+              >
+                Get Premium Plus
+              </Button>
             </div>
           </div>
         </div>
