f53ef617ab94b08002bffa325954d2958b64fb40
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-03-24 16:40:04 +0000

    Layout adjustments.

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="relative flex flex-col items-center justify-center gap-6 bg-slate-950 py-20 text-center text-gray-900 md:text-start lg:gap-12">
-      <Image
-        src="AILogo.svg"
-        alt=""
-        width={2000}
-        height={2000}
-        className="absolute -top-20 z-0 max-h-[125vh] opacity-5 lg:-top-60"
-      />
-      <Hero />
+      <section className="relative flex h-[70vh] flex-col items-center justify-center gap-6 lg:gap-12">
+        <Hero />
+      </section>
       <div className="relative mx-auto my-8 w-[95vw] max-w-[2000px] lg:w-[90vw]">
         <div className="absolute right-0 top-0 h-[60%] w-[60%] bg-[#0369A1] blur-[100px] md:blur-[200px] lg:blur-[400px]" />
         <div className="absolute bottom-0 left-0 h-[60%] w-[60%] bg-[#0D9488] blur-[100px] md:blur-[200px] lg:blur-[400px]" />
